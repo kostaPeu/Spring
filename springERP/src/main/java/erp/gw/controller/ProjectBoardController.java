69e7d9afa038aee2cee63fde1983f8ead89264a5
pado <pado@pado-PC>
2016-06-01 12:08:10 +0900

    csrf 추가

--- springERP/src/main/java/erp/gw/controller/ProjectBoardController.java
+++ springERP/src/main/java/erp/gw/controller/ProjectBoardController.java
@@ -6,5 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/groupware/pBoard")
 public class ProjectBoardController {
+	
+	public String mypBoardList(){
+		
+		return "";
+	}
 
 }
