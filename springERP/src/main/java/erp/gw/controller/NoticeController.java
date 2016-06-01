b2a2bc3148bff41c2235fb950ed343f06a09a184
pado <pado@pado-PC>
2016-06-01 17:46:49 +0900

    pull

--- springERP/src/main/java/erp/gw/controller/NoticeController.java
+++ springERP/src/main/java/erp/gw/controller/NoticeController.java
@@ -1,10 +1,12 @@
 package erp.gw.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import erp.gw.notice.domain.NoticeVO;
+import erp.hr.domain.EmployeeVO;
 
 @Controller
 @RequestMapping("/groupware/notice")
@@ -12,6 +14,15 @@ public class NoticeController {
 	
 	@RequestMapping(value="/notice_list", method = RequestMethod.GET)
 	public void listPage() throws Exception{
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		Object principal = auth.getPrincipal();
+		String name="";
+		if(principal != null && principal instanceof EmployeeVO){
+			name = ((EmployeeVO)principal).getE_name();
+		}
+		
+		System.out.println("name : "+name);
 	}
 	
 
--- springERP/src/main/java/erp/gw/projectboard/persistence/ProjectBoardDao.java
+++ springERP/src/main/java/erp/gw/projectboard/persistence/ProjectBoardDao.java
@@ -1,5 +1,15 @@
 package erp.gw.projectboard.persistence;
 
-public interface ProjectBoardDao {
+import java.util.List;
 
+import erp.gw.projectboard.domain.Criteria;
+import erp.gw.projectboard.domain.ProjectsVO;
+import erp.gw.projectboard.domain.SearchCriteria;
+
+public interface ProjectBoardDao {
+	  public List<ProjectsVO> listAll() throws Exception;
+	  public List<ProjectsVO> listCriteria(Criteria cri) throws Exception;
+	  public int countPaging(Criteria cri)throws Exception;
+	  public int listSearchCount(SearchCriteria cri)throws Exception;
+	  public List<ProjectsVO> listSearch(SearchCriteria cri)throws Exception;
 }
