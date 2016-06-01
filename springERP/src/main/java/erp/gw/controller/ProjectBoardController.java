3da4674c6543e0e1f6daec54598ca543fc442ccc
pado <pado@pado-PC>
2016-06-01 10:57:17 +0900

    pull

---
+++ springERP/src/main/java/erp/gw/controller/ProjectBoardController.java
@@ -0,0 +1,10 @@
+package erp.gw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/groupware/pBoard")
+public class ProjectBoardController {
+
+}
