346e04466d34ee4855222670eb965b29232a5940
pado <pado@pado-PC>
2016-06-10 16:49:27 +0900

    지각 반차

--- springERP/src/main/java/erp/login/persistence/LoginDao.java
+++ springERP/src/main/java/erp/login/persistence/LoginDao.java
@@ -19,4 +19,16 @@ public class LoginDao {
 	public void leave(String emp_id) throws Exception {
 		session.update(namespace + ".leave", emp_id);
 	}
+	
+	public void lateness(String emp_id) throws Exception{
+		session.update(namespace + ".late", emp_id);
+	}
+	
+	public void halfAnnual(String emp_id) throws Exception{
+		session.update(namespace + ".half", emp_id);
+	}
+	
+	public void dateReset(String emp_id) throws Exception{
+		session.update(namespace + ".reset", emp_id);
+	}
 }
